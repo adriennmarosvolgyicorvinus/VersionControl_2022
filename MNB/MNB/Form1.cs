3184004e622a86181fcee5af06f4c4c456215dee
p1mo9o <p1mo9o@SZA0030937>
2022-11-26 10:32:11 +0100

    3_4feladat

--- MNB/MNB/Form1.cs
+++ MNB/MNB/Form1.cs
@@ -1,21 +1,44 @@
-﻿using System;
+﻿using MNB.Entities;
+using MNB.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace MNB
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
+            GetRates();
+        }
 
+
+
+        private static void GetRates()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            GetExchangeRatesResponseBody response = (GetExchangeRatesResponseBody)mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            MessageBox.Show(result);
         }
     }
 }
