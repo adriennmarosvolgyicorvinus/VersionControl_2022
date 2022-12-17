8b8f0ade9f658389b7a5b378bb876a3be64882f5
p1mo9o <p1mo9o@SZA0030937>
2022-12-17 13:54:59 +0100

    feladat5

--- Value at Risk/Value at Risk/Form1.cs
+++ Value at Risk/Value at Risk/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Value_at_Risk.Entities;
 
 namespace Value_at_Risk
 {
@@ -15,12 +16,25 @@ namespace Value_at_Risk
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> ticks;
 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks;
 
+            CreatePortfolio();
+
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
