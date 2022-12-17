449043d12ea085b974a9071e49bda3d28dd26c73
p1mo9o <p1mo9o@SZA0030937>
2022-12-17 13:43:05 +0100

    feladat4b

--- Value at Risk/Value at Risk/Form1.cs
+++ Value at Risk/Value at Risk/Form1.cs
@@ -12,9 +12,15 @@ namespace Value_at_Risk
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> ticks;
+
         public Form1()
         {
             InitializeComponent();
+            ticks = context.Tick.ToList();
+            dataGridView1.DataSource = ticks;
+
         }
     }
 }
